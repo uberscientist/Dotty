ad89206b85a4c2df9948813d66184faebfe1ce6b
Simon Moon <simoon@gmx.com>
2012-02-17 20:14:44 -0700

    Too many updates to list

--- dotty.js
+++ dotty.js
@@ -1,34 +1,135 @@
 var socket = io.connect('http://localhost:4444');
 
-function place_circle(x,y,color){
+/**
+  Functions
+**/
+
+//Dot drawing function
+function place_circle(obj){
+  var x = obj.x,
+      y = obj.y,
+      color = obj.color;
   var ctx = $('canvas#dotty-canv')[0].getContext('2d');
   ctx.beginPath();
-  ctx.arc(x, y, 5, 0, Math.PI*2, true);
+  ctx.arc(x, y, 8, 0, Math.PI*2, true);
   ctx.closePath();
   ctx.fillStyle = '#'+color;
   ctx.fill();
 }
 
-function update(x,y,color){
-  socket.emit('place_dot',{x: x,
-                           y: y,
-                           color: color});
+//Add new artist DIV
+function add_artist(id){
+
+  //Select container DIV and append artist class div with unique ID
+  $('div#artist-display').append('<div class=\"artist\" id=\"'+ id + '\">');
 }
 
-socket.on('welcome', function(data){
+//Remove artist DIV
+function rm_artist(id){
+
+  //Select container DIV and  artist class div with unique ID
+  $('div#artist-display').remove();
+}
+
+
+//Update artist DIV
+function update_artist(obj){
+
+  //Select div with ID, and apply CSS
+  $('div.artist#'+obj.id).css('background-color', '#'+obj.color);
+}
+
+//Update server with new dot info
+function update(obj){
+  socket.emit('place_dot',{x: obj.x,
+                           y: obj.y,
+                           color: obj.color});
+}
+
+/**
+  Socket.IO events
+**/
+
+//Recieve NEXT signal from server
+socket.on('next', function(data){
+
 });
 
-socket.on('place_dot', function(data){
-  place_circle(data.x, data.y, data.color);
+
+//Recieve WELCOME from server
+socket.on('welcome', function(data){
+  id = data.id;
+
+  //Loop through dot data and draw canvas
+  for(var i = 0; i<data.dots.length; i++){
+    place_circle(JSON.parse(data.dots[i]));
+  }
+
+  //Loop through existing IDs
+  for each(var other_id in data.other_ids){
+    add_artist(other_id)
+  }
+
+  //append your marker to the end of the others
+  add_artist(id);
 });
+
+//Recieve dot broadcast from server
+socket.on('place_dot', function(data){
+
+  //Add circle to context
+  place_circle(data);
+});
+
+//New user connection
+socket.on('new_artist', function(data){
+  add_artist(data);
+});
+
+//User leave
+socket.on('leave', function(data){
+  rm_artist(data);
+});
+
+//Update color
+socket.on('update_color', function(data){
+  update_artist(data);
+});
+
+/**
+  DOM JavaScript
+**/
 
 $(document).ready(function(){
-  var canv = $('canvas#dotty-canv');
-  canv.click(function(e){
-    var x = e.clientX - canv.offset().left - 3;
-    var y = e.clientY - canv.offset().top - 5;
+
+  //Change cursor when hovering canvas
+  $('canvas#dotty-canv').hover(function(){
+    $(this).css('cursor', 'crosshair');
+  });
+  $('div.artist').hover(function(){
+    $(this).css('cursor', 'crosshair');
+  });
+
+  //Click event listener on canvas
+  $('canvas#dotty-canv').click(function(e){
+
+    //Canvas object
+    var canv = $('canvas#dotty-canv');
+
+    //Create dot info object
+    var obj = { x: e.clientX - canv.offset().left - 2,
+                y: e.clientY - canv.offset().top - 3,
+                color: $('input.color').val() };
+
+    place_circle(obj);
+    update(obj);
+  });
+
+  //Color picker change listener
+  $('input.color').change(function(){
     var color = $('input.color').val();
-    place_circle(x,y,color);
-    update(x,y,color);
+    obj = {color: color, id: id};
+    socket.emit('update_color', obj);
+    update_artist(obj);
   });
 });
--- dotty_sockets.js
+++ dotty_sockets.js
@@ -4,7 +4,7 @@ var io = require('socket.io').listen(4444),
 
 //Socket.IO setup
 //Lower default debug level to clean up console
-io.set('log level', 1)
+io.set('log level', 1);
 
 //Redis setup
 db = redis.createClient();
@@ -15,41 +15,74 @@ function md5(plaintext){
   return md5;
 }
 
+
+
 //On connection event
 io.sockets.on('connection', function(socket){
 
   //Get the user's IP address
-  var ip = socket.handshake.address.address,
+  var ip = socket.handshake.address.address;
 
   //Make MD5 hash of it
-      ip_hash = md5(ip);
+  var ip_hash = md5(ip);
 
   //Check if Redis has record of ID
   db.sismember('dotty:ids', ip_hash, function(err, data){
     if(data == 1){
       console.log('user already connected');
+      welcome(ip_hash);
     } else {
+      welcome(ip_hash);
+      socket.broadcast.emit('new_artist', ip_hash);
 
-      //Get connected user IDs
-      db.smembers('dotty:ids', function(err, data){
-        socket.emit('welcome', {'id': ip_hash,
-                                'other_ids': data});
-
-        //Add new ID to set
-        db.sadd('dotty:ids', ip_hash)
-      });
+      //Add new ID to set
+      db.sadd('dotty:ids', ip_hash);
     }
+  });
+/**
+  socket.IO events
+**/
+  //On update of color
+  socket.on('update_color', function(data){
+    //data.id and .color
+    socket.broadcast.emit('update_color', data);  
   });
 
   //On recieving new dot info
   socket.on('place_dot', function(data){
-    socket.broadcast.emit('place_dot', data);
+    var string_data = JSON.stringify(data)
+
+    //add dot info to dot set
+    db.sadd('dotty:dots',string_data , function(){
+
+      //Broadcast dot object to all connected
+      socket.broadcast.emit('place_dot', data);
+    });
+
+    //push dot info to the dot stack
+    db.lpush('dotty:dot-stack', string_data);
   });
 
   //On disconnect event
   socket.on('disconnect', function(){
 
     //Remove member from ip set
-    db.srem('dotty:ids', ip_hash);
+    db.srem('dotty:ids', ip_hash, function(){
+
+      //Let everyone know who left
+      socket.broadcast.emit('leave', ip_hash);
+    });
   });
+
+//Emit welcome data
+function welcome(ip_hash){
+    db.smembers('dotty:ids', function(err, ids){
+      db.smembers('dotty:dots', function(err, dots){
+        socket.emit('welcome', {'id': ip_hash,
+                                'other_ids': ids,
+                                'dots': dots});
+      });
+  });
+}
+
 });
